ff27395cb14c64727ea4e7c5df6d39d83fe8276b
adam wong <adamwong246@gmail.com>
2020-07-21 14:22:30 -0700

    performance

--- paths2.rb
+++ paths2.rb
@@ -1,16 +1,28 @@
 def findSubPath (path, subpath)
   # puts "findSubPath: (#{path}, #{subpath})"
 
-  p = path.join('')
-  sp = subpath.join('')
+  # subpath.each{ |spc|
+  #
+  # }
+  # p = path.to_s
+  # sp = subpath.to_s
 
-  if p.include? sp
-    # puts "TRUE"
-    return true
-  else
-    # puts "FALSE"
-    return false
+  # if p.include? sp
+  #   # puts "TRUE"
+  #   return true
+  # else
+  #   # puts "FALSE"
+  #   return false
+  # end
+
+  for ndx in (0...path.length-3)
+    subsubpath = path.slice(ndx, 4)
+    if subsubpath == subpath
+      return true
+    end
   end
+
+  return false
 
 end
 
@@ -21,11 +33,12 @@ def mostCommon4PagePath (paths)
 
   # puts paths.inspect
 
-  mostcommon = []
-  occurences = 0
+  mostcommon = ''
+  mostOccurences = 0
+  candidates = {}
 
-  paths.each { |path|
-    # puts ""
+  paths.each_with_index { |path, z|
+    # puts "#{z}/#{paths.length}"
     # puts path.inspect
     # puts occurences
 
@@ -35,24 +48,33 @@ def mostCommon4PagePath (paths)
       # puts "subpath: #{subpath.inspect}"
 
 
-      occurences2 = 0
-      paths.each { |path2|
+      if !candidates[subpath]
+        # occurences = 0
+        candidates[subpath] = 0
 
-        if findSubPath(path2, subpath)
-          # puts "subpath found"
-          # puts path2.inspect
-          # puts subpath.inspect
-          occurences2 = occurences2 + 1
-        else
-          # puts "subpath NOT found"
+        paths.each { |path2|
+          found = false
+
+          if findSubPath(path2, subpath) && found == false
+            # puts "subpath found"
+            # puts path2.inspect
+            # puts subpath.inspect
+            candidates[subpath] = candidates[subpath] + 1
+            found = true
+          else
+            # puts "subpath NOT found"
+          end
+        }
+
+        if (candidates[subpath] > mostOccurences)
+          mostOccurences = candidates[subpath]
+          mostcommon = subpath
         end
+        # occurences = occurences2
+        # mostcommon = subpath
 
-
-      }
-
-      if (occurences2 > occurences)
-        occurences = occurences2
-        mostcommon = subpath
+      else
+        # puts "#{subpath} already was checked"
       end
 
 
@@ -60,7 +82,7 @@ def mostCommon4PagePath (paths)
   }
 
   puts "Most common 4 page path: p#{mostcommon.join(',p')}"
-  puts "Number of occurences: #{occurences.to_i()}"
+  puts "Number of occurences: #{candidates[mostcommon].to_i()}"
 end
 
 def cleanInput (paths)
