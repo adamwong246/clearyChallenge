aa7f6e57fe5cae37bf5f0f1f266d38e101f9c840
adam wong <adamwong246@gmail.com>
2020-07-21 12:19:41 -0700

    clean input

--- paths2.rb
+++ paths2.rb
@@ -1,6 +1,12 @@
 def mostCommon4PagePath (paths)
-  puts paths
+  puts paths.inspect
+end
+
+def cleanInput (paths)
+  paths.map { |s0|
+    s0.split(',').map{ | s1 | s1.gsub('p', '').gsub("\n", '')}
+  }
 end
 
 paths = File.readlines('paths2.txt')
-mostCommon4PagePath(paths)
+mostCommon4PagePath(cleanInput(paths))
